2da2a0c85a2977f6085dfa1fd7d13d2c54848380
shambhuraje88-git <shambhurajepawar8@gmail.com>
2025-05-06 13:53:58 +0530

    Update outputs.tf

--- terraform-manifests/modules/infra_services/outputs.tf
+++ terraform-manifests/modules/infra_services/outputs.tf
@@ -1,8 +1,40 @@
+resource "aws_security_group" "terraform_test_sg" {
+  name        = "${var.cloud_env}_terraform_test_sg"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.terraform_test_vpc.id
 
-output "security_group_id" {
-  value       = aws_security_group.terraform_test_sg.id
-  description = "Security Group ID"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.access_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.cloud_env}_terraform_test_sg"
+  }
 }
+
+
+
+
+
+
+
+
+
+
+#output "security_group_id" {
+ # value       = aws_security_group.terraform_test_sg.id
+  #description = "Security Group ID"
+#}
 
 // You can access module output variables using the following syntax:
 // module.<MODULE_NAME>.<OUTPUT_NAME>
